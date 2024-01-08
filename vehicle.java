3b9b65ef1b4c986f8d3cdda0bd200ce6488b8c82
sakey <152389191+sakey01@users.noreply.github.com>
2024-01-08 15:56:56 +0000

    Update vehicle.java

--- vehicle.java
+++ vehicle.java
@@ -1,5 +1,16 @@
+
 abstract class vehicle{
-    
+    protected int age = 4;
+    public static void main(String[] args) {
+        car c = new car();
+        bike b = new bike(); 
+        lorry l = new lorry();
+
+        b.drive();
+        c.drive();
+        l.drive();
+        System.out.println(l.age);
+    }
 }
 class car extends vehicle {
     void drive(){
@@ -10,4 +21,11 @@ class bike extends vehicle {
     void drive(){
         System.out.println("driving bike");
     }
+}
+class lorry extends vehicle{
+    void drive(){
+        System.out.println("driving lorry");
+        
+    }
+
 }
