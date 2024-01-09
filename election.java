9ee90228e98ba714639c02ad89d8333532bab008
sakey <152389191+sakey01@users.noreply.github.com>
2024-01-09 17:37:42 +0000

    Update election.java

--- election.java
+++ election.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 
+
 public class Election{
     public static void main(String[] args) {
         ArrayList<String> votes = new ArrayList<>();
@@ -27,7 +28,14 @@ public class Election{
         }
         System.out.println(votes);
         System.out.println(count);
-        int highest = Collections.max(count);
-        System.out.println("the winner is " + votes.get(highest) + " with " + highest);
-    }    
+
+        int highestVote = Collections.max(count);
+        int x = 0;
+        for (int k = 0; k < votes.size(); k++){
+            if (highestVote == count.get(k)){
+                ++x;
+                System.out.println(votes.get(k) + " is the winner with " + highestVote + " vote(s)");
+            }
+        }
+    }       
 }
