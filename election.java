c5524b1454c8f8839d14c3cd9399d9addfacc38e
sakey <152389191+sakey01@users.noreply.github.com>
2024-01-09 18:02:55 +0000

    Update election.java
    still has errors when matching variables

--- election.java
+++ election.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 
-
+// Figure 4, source code for election (task 4)
 public class Election{
     public static void main(String[] args) {
+        // initialisation
         ArrayList<String> votes = new ArrayList<>();
         ArrayList<Integer> count = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
@@ -12,30 +13,41 @@ public class Election{
         while (true) {
             System.out.println("Enter vote: ");
             String input = sc.nextLine();
+            // allows the user to exit the loop
             if (input.equals("q") || input.isEmpty()){
                 System.out.println("END");
                 break;
             }
+            // obtains the index of input if already the element is present votes
+            // and increment the corresponding index in count
             if (votes.contains(input)){
                 int j = votes.indexOf(input);
                 int i = count.get(j) + 1;
                 count.set(j,i);
             }
+            // adds the input if the element is not already present in the array
             else {
                 votes.add(input);
                 count.add(1);
             }    
         }
-        System.out.println(votes);
-        System.out.println(count);
-
+        // using collections, the highest vote is stored
         int highestVote = Collections.max(count);
-        int x = 0;
+        int x = 1;
+        int y = 0;
         for (int k = 0; k < votes.size(); k++){
+            System.out.println(votes.get(k) + " recieved " + count.get(k) + " votes");
+            // if there are multiple highest votes then the x is incremented
             if (highestVote == count.get(k)){
                 ++x;
-                System.out.println(votes.get(k) + " is the winner with " + highestVote + " vote(s)");
+                y = k;
             }
+        }
+        if (x == 1) {
+            System.out.println(votes.get(y) + " is the winner with " + highestVote + " vote(s)");
+        }    
+        else if (x>1) {
+            System.out.println("the votes are tied");
         }
     }       
 }
