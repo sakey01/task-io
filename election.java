b2c7fa211c452fbbe5e5f2a0d7d108ee1028138a
sakey <152389191+sakey01@users.noreply.github.com>
2024-01-12 19:56:49 +0000

    Update election.java

--- election.java
+++ election.java
@@ -1,11 +1,14 @@
+// Sheikh Ahmed, 33114212, programming, B.S.c Computer Science, Ikram Rehman
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 
-// Figure 4: source code for election (task 4)
+/* election takes the users unique input, stores it in votes. It also has an array list called count
+which keeps track of how many votes each candidate recieves. finally the person with the highest votes
+is returned */
 public class Election{
     public static void main(String[] args) {
-        // initialisation
+        // initialisation votes and count of type array list
         ArrayList<String> votes = new ArrayList<>();
         ArrayList<Integer> count = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
@@ -13,7 +16,7 @@ public class Election{
         while (true) {
             System.out.println("Enter vote: ");
             String input = sc.nextLine();
-            // allows the user to exit the loop
+            // enables the user to exit the loop
             if (input.equals("q") || input.isEmpty()){
                 System.out.println("END");
                 break;
@@ -31,23 +34,25 @@ public class Election{
                 count.add(1);
             }    
         }
+        sc.close();
         // using collections, the highest vote is stored in 'highestVote'
         int highestVote = Collections.max(count);
         int x = 0;
         int y = 0;
         // goes through count and stores the index of the highest vote
         for (int k = 0; k < votes.size(); k++){
-            System.out.println(votes.get(k) + " recieved " + count.get(k) + " votes");
+            System.out.println(votes.get(k) + " recieved " + count.get(k) + " vote(s)");
             // if there are multiple highest votes then x is incremented
             if (highestVote == count.get(k)) {
                 x++;
                 y = k;
             }
         }
-        // prints the person with the highest votes if they are unmatched
+        // prints the person with the highest votes
         if (x == 1) {
-            System.out.println(votes.get(y) + " is the winner with " + highestVote + " votes(s)");
+            System.out.println(votes.get(y) + " is the winner with " + highestVote + " votes");
         }
+        // the following is printed if 2 or more candidates recieve the highest votes
         else {
             System.out.println("the votes are tied");
         }
