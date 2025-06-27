95f2c833c598dd608758d7be3309ef80dedf284b
Sakey <sheikh36ahmed@gmail.com>
2025-06-27 11:05:08 +0100

    initial commit

---
+++ to-do-list/main.js
@@ -0,0 +1,25 @@
+/*
+Create a simple to-do list app that allows users to search, add, edit, and delete items. Use local storage to store the data.
+
+Create a simple webpage where user can put in anyone's GitHub username and see their profile information. You can use GitHub's API to fetch the data. For example, here is the sample URL to fetch my data. Make sure to add validation and error handling.
+
+Create a basic calculator app that allows users to perform basic arithmetic operations.
+*/
+
+//search
+
+
+//add
+const list = document.querySelector("#list");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function() {
+    console.log("h");
+
+})
+
+
+//edit
+
+
+//delete
