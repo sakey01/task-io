4f02f01f14eb343e216ff3747432cead9addd2ca
Sakey01 <sheikh36ahmed@gmail.com>
2025-06-28 01:55:16 +0100

    backend function

--- main.js
+++ main.js
@@ -11,25 +11,28 @@ addTask.addEventListener("keydown", function(e) {
     if(e.key === "Enter") addbtn.click();
 });
 
-currItem.addEventListener("click", () => {
-            currItem.style.textDecoration = "line-through";
-            currItem.style.backgroundColor = "grey";
-        });
-
 //add
  addbtn.addEventListener("click", () => {
     const text = addTask.value.trim();
     console.log(typeof(lidt));
 
-
     if (text !== "") {
+        const currItem = document.createElement("li");
         currItem.innerHTML = text;
         list.append(currItem);
         addTask.value = "";
 
         currItem.style.overflow = "hidden";
 
-        
+        currItem.addEventListener("click", () => {
+            if (currItem.style.textDecoration === "line-through") {
+                currItem.style.textDecoration = "none";
+                currItem.style.backgroundColor = "transparent";
+            } else {
+                currItem.style.textDecoration = "line-through";
+                currItem.style.backgroundColor = "grey";
+            }
+        });
 
         //delete
         const delbtn = document.createElement("button");
@@ -69,10 +72,15 @@ currItem.addEventListener("click", () => {
     } 
 }); 
 
-
-
- 
-
-
 //search
+const search = document.querySelector("#search-icon");
+const nav = document.querySelector("nav");
 
+search.addEventListener("click", () => {
+    if (!document.querySelector("#search-bar")) {
+        const searchBar = document.createElement("input");
+        searchBar.id = "search-bar";
+        searchBar.placeholder = "Type here...";
+        nav.append(searchBar);
+    }
+});
