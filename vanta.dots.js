00c2c17af9e08b49bdcfa4c33610675a39bd6aae
Sakey01 <sheikh36ahmed@gmail.com>
2025-06-28 02:21:58 +0100

    bg update

---
+++ vanta.dots.js
@@ -0,0 +1,16 @@
+VANTA.DOTS({
+  el: "html",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xf9f6f6,
+  color2: 0xffffff,
+  backgroundColor: 0x4a4a4a,
+  size: 2.00,
+  spacing: 15.00,
+  showLines: false
+})
